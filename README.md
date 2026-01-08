382351a608bc50dcbe2ecc3c29918d5b3384de1f
Michael <mrenn2000@gmail.com>
2026-01-08 12:11:17 +0100

    Add readme stuff add script

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # soda-core-postgresql
 Quickstart for Soda Core /w PostgreSQL
+
+## Commands
+- Use `docker compose up` in order to start the DB and run a scan with soda core.
+
+### Database 
+- Use `docker exec -it <id> /bin/bash` to run bash in postgresql. 
+- Then use `psql -U soda -d postgres` to connect to the DB.
+- Use `\dt` to show all tables
+- If there is **no data** use the `init_db.sql` script to add demo data to postgres.
---
+++ init_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user_data (
+    id SERIAL PRIMARY KEY,
+    vorname VARCHAR(50),
+    name VARCHAR(50),
+    birthday DATE,
+    strasse VARCHAR(100),
+    stadt VARCHAR(50),
+    zip VARCHAR(10)
+);
+INSERT INTO user_data (vorname, name, birthday, strasse, stadt, zip)
+VALUES 
+('Max', 'Mustermann', '1985-05-20', 'Hauptstraße 1', 'Berlin', '10115'),
+('Erika', 'Schmidt', '1992-11-03', 'Goethestraße 42', 'München', '80331'),
+('Thomas', 'Müller', '1978-02-15', 'Feldweg 7', 'Hamburg', '20095'),
+('Sarah', 'Wagner', '2000-08-25', 'Am Hang 12', 'Köln', '50667'),
+('Andreas', 'Bauer', '1965-12-30', 'Marktplatz 3', 'Leipzig', '04109');"
